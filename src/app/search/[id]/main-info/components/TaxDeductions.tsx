b9cf2d167f42f7a981fab90ca192e66ce903a3b6
sogeking7 <kairolla222@gmail.com>
2024-12-03 15:32:15 +0500

    feat: data fetch add

--- src/app/search/[id]/main-info/components/TaxDeductions.tsx
+++ src/app/search/[id]/main-info/components/TaxDeductions.tsx
@@ -1,53 +1,66 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-
-export const description = "A bar chart";
-
-const chartData = [
-  { year: "2015", val: 20 },
-  { year: "2016", val: 30 },
-  { year: "2017", val: 40 },
-  { year: "2018", val: 50 },
-  { year: "2019", val: 60 },
-  { year: "2020", val: 70 },
-  { year: "2021", val: 80 },
-  { year: "2022", val: 90 },
-  { year: "2023", val: 100 },
-  { year: "2024", val: 90 },
-];
+import { useParams } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CompanyService } from "@/features/company/api/company.service";
+import { useQuery } from "@tanstack/react-query";
+import { calculateTotalSum, formatKZT } from "@/lib/utils";
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
+  summa: {
+    label: "Сумма",
     color: "hsl(var(--chart-1))",
   },
 } satisfies ChartConfig;
 
 export const TaxDeductions = () => {
+  const { id } = useParams();
+
+  const company_bin = id as string;
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ["get-company-by-bin", { company_bin }],
+    queryFn: async () => await CompanyService().findByBin(company_bin),
+    refetchOnWindowFocus: false,
+    refetchIntervalInBackground: false,
+    refetchInterval: false,
+  });
+
+  if (isPending) {
+    return <Skeleton className="h-[300px] w-full rounded-2xl bg-white" />;
+  }
+
+  if (error) return <>{"An error has occurred: " + error.message}</>;
+
+  // ! Uncomment
+  if (!data.success) {
+    return <p className="max-md:my-5 mt-5 font-semibold">{data.data}</p>;
+  }
+  const { dynamic_tax_records } = data.data.tax_info;
+
+  const chartData = Object.entries(dynamic_tax_records).map(
+    ([year, { summa }]) => ({
+      year,
+      summa,
+    })
+  );
+
   return (
     <Card className="bg-white !rounded-2xl flex flex-col border-none">
       <CardHeader>
         <CardTitle className="!text-xl">Налоговые отчисления</CardTitle>
         <div className="flex flex-col gap-1 mt-4">
           <p className="text-info text-sm">Всего налоговых отчислений:</p>
-          <b>464 118 416 686 KZT</b>
+          <b>{formatKZT(calculateTotalSum(dynamic_tax_records))}</b>
         </div>
       </CardHeader>
       <CardContent className="!pt-0">
@@ -69,7 +82,7 @@ export const TaxDeductions = () => {
               cursor={false}
               content={<ChartTooltipContent hideLabel />}
             />
-            <Bar dataKey="val" fill="#77BD8B" radius={8} className="w-10" />
+            <Bar dataKey="summa" fill="#77BD8B" radius={2} className="w-7" />
           </BarChart>
         </ChartContainer>
       </CardContent>
